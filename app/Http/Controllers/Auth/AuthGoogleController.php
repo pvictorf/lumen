47e17a93188abeaa6c33d1495cd92fa7910ffb62
pvictorf <pvictorferreira@hotmail.com>
2021-11-08 18:43:47 -0300

    Removed email from query
    Removed the email from the query to verify if user exists in database, because
     the user can change their email from Facebook or Google.

--- app/Http/Controllers/Auth/AuthGoogleController.php
+++ app/Http/Controllers/Auth/AuthGoogleController.php
@@ -23,7 +23,6 @@ class AuthGoogleController extends Controller
 
       $user = User::where([
         'google_id' => $social->id,
-        'email' => $social->email,
       ])->first();
 
       if(!$user) {
