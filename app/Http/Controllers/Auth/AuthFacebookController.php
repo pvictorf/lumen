f4faede85194ffe66c2474cfeb6878efce9b3019
pvictorf <pvictorferreira@hotmail.com>
2021-11-30 16:20:11 -0300

    Add userFromToken authentication for mobile apps

--- app/Http/Controllers/Auth/AuthFacebookController.php
+++ app/Http/Controllers/Auth/AuthFacebookController.php
@@ -11,29 +11,19 @@ use Laravel\Socialite\Facades\Socialite;
 
 class AuthFacebookController extends Controller
 {
+
   public function redirect(Request $request)
   {
     return Socialite::driver('facebook')->stateless()->redirect();
   }
+
 
   public function callback(Request $request)
   {
     try {
       $social = Socialite::driver('facebook')->stateless()->user();
 
-      $user = User::where([
-        'facebook_id' => $social->id,
-      ])->first();
-
-      if(!$user) {
-        $user = User::create([
-          'facebook_id' => $social->id,
-          'name' => $social->name,
-          'email' => $social->email,
-          'avatar' => $social->avatar,
-          'password' => Hash::make(random_bytes(10)),
-        ])->assignRole('user');
-      }
+      $user = $this->createOrLoginUser($social);
 
       return AuthController::authenticateAndResponse($user);
 
@@ -43,5 +33,41 @@ class AuthFacebookController extends Controller
         "error" => $th->getMessage(),
       ], 400);
     }
+  }
+
+  // https://stackoverflow.com/questions/65142862/log-in-users-in-flutter-through-social-accounts-with-laravel-socialite-backend?answertab=active#tab-top
+  public function authenticate(Request $request) 
+  {
+    $this->validate($request, [
+      'token' => 'required|string'
+    ]);
+
+    $token = $request->token;
+
+    $social = Socialite::driver('facebook')->stateless()->userFromToken($token);
+
+    $user = $this->createOrLoginUser($social);
+
+    return AuthController::authenticateAndResponse($user);
+  }
+
+
+  private function createOrLoginUser($social) 
+  {
+    $user = User::where([
+      'facebook_id' => $social->id,
+    ])->first();
+
+    if(!$user) {
+      $user = User::create([
+        'facebook_id' => $social->id,
+        'name' => $social->name,
+        'email' => $social->email,
+        'avatar' => $social->avatar,
+        'password' => Hash::make(random_bytes(10)),
+      ])->assignRole('user');
+    }
+
+    return $user;
   }
 }
