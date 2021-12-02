931bce7458b579326ebbf2da75843df6a08426e3
pvictorf <pvictorferreira@hotmail.com>
2021-12-02 11:17:50 -0300

    Add 401 Unauthorized if token is invalid

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -44,11 +44,11 @@ class Authenticate
         } catch (\Exception $exception) {
 
             if ($exception instanceof TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
+                return response()->json(['status' => 'Token is Invalid'], 401);
             } else if ($exception instanceof TokenExpiredException) {
-                return response()->json(['status' => 'Token is Expired']);
+                return response()->json(['status' => 'Token is Expired'], 401);
             } else {
-                return response()->json(['status' => 'Authorization Token not found']);
+                return response()->json(['status' => 'Authorization Token not found', 401]);
             }
         }
 
